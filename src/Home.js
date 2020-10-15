0d383bcc799b541c0136964ae54ccb6aace90884
Sourav das <70948672+freakflames29@users.noreply.github.com>
2020-10-15 16:43:55 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Hero from "./Hero";
 import Category from "./Category";
+import Featuredproducts from "./Featuredproducts";
 
 function Home() {
 	return (
@@ -9,6 +10,7 @@ function Home() {
 			<Header />
 			<Hero />
 			<Category />
+			<Featuredproducts />
 
 		</div>
 	);
