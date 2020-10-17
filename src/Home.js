61e497014fae9ae2a5d75b9e1a9997113031fbe8
SukantaMajhi <majhisukanta48@gmail.com>
2020-10-18 04:08:33 +0530

    Changes

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import Header from './Header'
 import Hero from './Hero'
@@ -14,24 +13,6 @@ function Home() {
             <Featuredproducts />
         </div>
     )
-=======
-import React from "react";
-import Header from "./Header";
-import Hero from "./Hero";
-import Category from "./Category";
-import Featuredproducts from "./Featuredproducts";
-
-function Home() {
-	return (
-		<div>
-			<Header />
-			<Hero />
-			<Category />
-			<Featuredproducts />
-
-		</div>
-	);
->>>>>>> ad09a3c9cd7dea79b2c2e22eb77aa2b7b4e63ec1
 }
 
 export default Home
