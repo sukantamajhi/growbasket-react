a2ebd686df9fabd6711a47dd7154c1cadd267d1a
Sukanta-Majhi-prog <sukantamajhi799@gmail.com>
2020-11-16 11:59:12 +0530

    Enter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-// import Header from './Header';
 import Product from './Product';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import SignIn from './Signin';
-// import { ContactMail } from '@material-ui/icons';
 import Contact from './Contact';
-// import Footer from './Footer';
 
 function App() {
 	return (
